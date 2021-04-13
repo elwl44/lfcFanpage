8e953873859136c1a0300b98f2867fc50f2bb112
elwl44 <pbk11908@gmail.com>
2021-04-14 02:43:08 +0900

    회원가입시 가입축하메일 발송

--- src/main/java/com/example/lfcFan/service/MemberService.java
+++ src/main/java/com/example/lfcFan/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.lfcFan.service;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.lfcFan.dao.MemberDao;
@@ -11,17 +12,38 @@ import com.example.lfcFan.util.Util;
 
 @Service
 public class MemberService {
+	@Value("${custom.siteName}")
+	private String siteName;
+
+	@Value("${custom.siteMainUri}")
+	private String siteMainUri;
+	
 	@Autowired
 	private MemberDao memberDao;
+	
+	@Autowired
+	private MailService mailService;
 
 	public int join(Map<String, Object> param) {
 		memberDao.join(param);
 
 		int id = Util.getAsInt(param.get("id"));
+		
+		sendJoinCompleteMail((String) param.get("email"));
 
 		return id;
 	}
 
+	private void sendJoinCompleteMail(String email) {
+		String mailTitle = String.format("[%s] 가입이 완료되었습니다.", siteName);
+
+		StringBuilder mailBodySb = new StringBuilder();
+		mailBodySb.append("<h1>가입이 완료되었습니다.</h1>");
+		mailBodySb.append(String.format("<p><a href=\"%s\" target=\"_blank\">%s</a>로 이동</p>", siteMainUri, siteName));
+
+		mailService.send(email, mailTitle, mailBodySb.toString());
+	}
+	
 	public boolean isJoinAvailableLoginId(String loginId) {
 		Member member = memberDao.getMemberByLoginId(loginId);
 
